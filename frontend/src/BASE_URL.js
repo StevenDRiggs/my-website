d464090bfa63e2b0d668dbd81d73bdf2684d3b6d
StevenDRiggs <sfriggs21@gmail.com>
2020-12-15 11:41:59 -0700

    begin Portfolio

---
+++ frontend/src/BASE_URL.js
@@ -0,0 +1,4 @@
+const BASE_URL = 'localhost:3000'
+
+
+export default BASE_URL
--- frontend/src/containers/Portfolio.js
+++ frontend/src/containers/Portfolio.js
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom'
+
+//import CIL from '../images/CIL.svg'
+
+import BASE_URL from '../BASE_URL'
+
+
 const Portfolio = () => {
   return (
     <div>
+      <Link to='#'> {/* to=`${BASE_URL}/portfolio/CIL` */}
+        <img src='https://picsum.photos/256' alt='Calapitter Image Library' /> {/* src={CIL} */}
+      </Link>
     </div>
   )
 }

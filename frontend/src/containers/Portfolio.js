aab2fb2d2830dd4358fbbe76f6e6127b393ee9ef
StevenDRiggs <sfriggs21@gmail.com>
2020-12-16 15:10:10 -0700

    added CIL to Portfolio

--- frontend/src/containers/Portfolio.js
+++ frontend/src/containers/Portfolio.js
@@ -1,16 +1,26 @@
 import { Link } from 'react-router-dom'
 
-//import CIL from '../images/CIL.svg'
+import { ReactComponent as CIL } from '../images/caterpillar.svg'
 
 import BASE_URL from '../BASE_URL'
+
+import './Portfolio.css'
 
 
 const Portfolio = () => {
   return (
     <div>
-      <Link to='#'> {/* to=`${BASE_URL}/portfolio/CIL` */}
-        <img src='https://picsum.photos/256' alt='Calapitter Image Library' /> {/* src={CIL} */}
-      </Link>
+      <div className='thumbnail'>
+        <Link to='#'> {/* to=`${BASE_URL}/portfolio/CIL` */}
+          <CIL className='thumbnail-image' alt='Calapitter Image Library' viewBox='-20 0 280 300' />
+        </Link>
+        <div className='github'>
+          <a href='https://github.com/StevenDRiggs/featured-projects.git' target="_blank">
+            <img src='https://github.githubassets.com/favicons/favicon.svg' alt='See Calapitter Image Library on GitHub' />
+          </a>
+        </div>
+        <caption className='thumbnail-caption'>Calapitter Image Library</caption>
+      </div>
     </div>
   )
 }
